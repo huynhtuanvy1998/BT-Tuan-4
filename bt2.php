97bac51b55d757bb52c613d2cf1f1cad99cf0c9d
BT Tuan 4 <37360403+huynhtuanvy1998@users.noreply.github.com>
2018-03-23 14:14:20 +0700

    Bai tap tuan 4

---
+++ bt2.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8"/>
+<form action="timkiem.php" method="POST">
+    <input type="text" name="tukhoa"/>  
+    <button type="submit" value="submit"/> Tìm </button>
+
+</form>
+
---
+++ timkiem.php
@@ -0,0 +1,3 @@
+<?php
+echo "ket qua: ".$_POST['tukhoa'];
+
